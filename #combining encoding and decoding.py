1825a8e802974964b794f0336fe400c76589ff1d
Lacking-some-wut-of-worth <150485434+Lacking-some-wut-of-worth@users.noreply.github.com>
2025-12-14 09:19:40 +0200

    combining

---
+++ #combining encoding and decoding.py
@@ -0,0 +1,11 @@
+#combining encoding and decoding 
+choose = 28
+while choose == 28:
+    print("--------------------------------")
+    print("Enter what you wish to do")
+    print("1: hiding a secret message into an image")
+    print("2: extract a secret message from an image")
+    choose = input(int("input your choise"))
+
+if choose == 1: print("You choose hiding a secret message into an immage")    
+if choose == 2: print("You choose extracting a secret message from an immage")    
